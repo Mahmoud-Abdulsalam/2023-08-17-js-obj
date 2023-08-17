130159ebc592a32bf0c1cf5f5682063818a40d21
Mahmoud Abdulsalam <abdulsalam.m@gmx.de>
2023-08-17 11:10:26 +0200

    new rep-fi

---
+++ demo.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "mahmoud",
+  age: 33,
+  job: "developer",
+  address: {
+    street: "wolfstreet",
+    homeNumber: "29",
+    zipcode: 53111,
+  },
+};
+console.log(person);
+console.log(person["address"]["zipcode"]);
